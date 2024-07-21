8e32144dde52e4b06c7f4d616197bdb7bcb7c841
saifahmad0622 <saifahmad0622@gmail.com>
2024-07-21 22:03:58 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo "hello world";
+//hello
+echo  4 + 6;
+echo "hi";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>hi</div>
+</body>
+
+</html>
---
+++ variable.php
@@ -0,0 +1,7 @@
+<?php
+//Varaible in php.
+$name = "Saif";
+$Name = "Harry";
+$income = 0;
+echo "My name is $name and My income is $income<br>";
+echo "My name is $Name and My income is NULL<br>";
